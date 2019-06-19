eaadf2d278fee1a49261220a27e6657de4bcab5c
Joshua Kang <joshua_kang@apple.com>
2019-06-18 20:07:02 -0700

    added Jian Yang just for giggles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,9 @@
     <div>
 
         <h1> Trivia </h1>
+        <div>
+            <b-img src="https://sound.peal.io/ps/covers/000/007/488/medium/hqdefault.jpg?1517036605" fluid alt="Responsive image"></b-img>
+        </div>
         <b-nav tabs>
             <b-nav-item disabled><b>Ten Question Quiz</b></b-nav-item>
             <b-nav-item disabled>Number of correct Answers: 0/10 </b-nav-item>
